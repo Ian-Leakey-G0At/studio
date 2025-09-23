dbb12756314bc1978b43defb7e87efbecc5ae73a
Ian Leakey <ian19leakey@gmail.com>
2025-09-23 02:01:08 +0000

    TRY AGain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Inter, Montserrat } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Header } from '@/components/header';
