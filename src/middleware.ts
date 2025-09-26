1520ce8feb993d7f2dc77fda6d1d484b3d437e1b
Ian Leakey <ian19leakey@gmail.com>
2025-09-26 12:42:12 +0000

    Try fixing this error: `Runtime Error: Error: The edge runtime does not

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getAuth } from '@/lib/firebase/adminApp';
 
+export const runtime = 'nodejs';
+
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const sessionCookie = request.cookies.get('__session')?.value;
